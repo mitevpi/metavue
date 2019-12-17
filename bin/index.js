e4bc62bee9453c52999b1b22eb961e5826f036a5
Petr Mitev <p.mitevpi@gmail.com>
2019-12-17 10:00:05 -0500

    refactor & release

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,24 @@
 const yargs = require("yargs");
 const metavue = require("../dist/index");
 
-const options = yargs.usage("Usage: -d <directory>").option("d", {
-  alias: "directory",
-  describe: "Root directory of the Vue application",
-  type: "string",
-  demandOption: true
-}).argv;
+const options = yargs
+  .version()
+  .example(
+    `metavue -d /Users/mitevpi/GitHub/stroll-app`,
+    "Run metavue in the root directory of the Vue application"
+  )
+  .usage("Usage: -d <directory>")
+  .help("h")
+  .alias("h", "help")
+  .option("d", {
+    alias: "directory",
+    describe: "Root directory of the Vue application",
+    type: "string",
+    demandOption: true
+  })
+  .epilogue(
+    "for more information, find the documentation at https://www.npmjs.com/package/metavue"
+  ).argv;
 
 const dir = options.directory;
 
